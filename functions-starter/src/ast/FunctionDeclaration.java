b1941b2b2066ebded60ef30bee8fbaa930fa8233
Graham Bing <grrraham@gmail.com>
2016-01-28 17:15:17 -0800

    created FunctionDeclaration AST class

---
+++ functions-starter/src/ast/FunctionDeclaration.java
@@ -0,0 +1,27 @@
+package ast;
+
+import visitor.Visitor;
+
+public class FunctionDeclaration extends Statement {
+	
+	public final Type type;
+	public final String name;
+	public final FormalList parameters;
+	public final NodeList body;
+	public final Expression returnVal;
+	
+	public FunctionDeclaration(Type type, String name, FormalList parameters, NodeList body, Expression returnVal) {
+		super();
+		this.type = type;
+		this.name = name;
+		this.parameters = parameters;
+		this.body = body;
+		this.returnVal = returnVal;
+	}
+
+	@Override
+	public <R> R accept(Visitor<R> v) {
+		return v.visit(this);
+	}
+
+}
