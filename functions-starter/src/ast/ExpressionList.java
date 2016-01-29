7e344cd3f3db93f840646e666d8df54c55299064
Graham Bing <grrraham@gmail.com>
2016-01-28 21:27:26 -0800

    reimplement ExpressionList to be more like NodeList

--- functions-starter/src/ast/ExpressionList.java
+++ functions-starter/src/ast/ExpressionList.java
@@ -1,17 +1,35 @@
 package ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import visitor.Visitor;
 
 public class ExpressionList extends AST {
 
-	public final NodeList<Expression> es;
+	private NodeList<Expression> exps;
 	
-	public ExpressionList(NodeList<Expression> es) {
-		this.es = es;
+	public ExpressionList(NodeList<Expression> exps) {
+		this.exps = exps;
+	}
+
+	public ExpressionList() {
+		this.exps = new NodeList<Expression>();
 	}
 
 	@Override
 	public <R> R accept(Visitor<R> v) {
 		return v.visit(this);
+	}
+		
+	public void add(Expression t) {
+		this.exps.add(t);
+	}
+	public int size() {
+		return exps.size();
+	}
+
+	public Expression elementAt(int i) {
+		return exps.elementAt(i);
 	}
 }
